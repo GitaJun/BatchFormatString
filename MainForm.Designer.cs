51a4d6e713c2b5fc56006fa4e362504993c46719
FJun <50333046+GitaJun@users.noreply.github.com>
2021-06-03 15:12:17 +0800

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace BatchString
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_pinyin = new System.Windows.Forms.CheckBox();
+            this.bt_excute = new System.Windows.Forms.Button();
+            this.groupBox_Output = new System.Windows.Forms.GroupBox();
+            this.tb_output = new System.Windows.Forms.TextBox();
+            this.groupBox_Var = new System.Windows.Forms.GroupBox();
+            this.tb_var = new System.Windows.Forms.TextBox();
+            this.groupBox_SQL = new System.Windows.Forms.GroupBox();
+            this.cbb_template = new System.Windows.Forms.ComboBox();
+            this.bt_selfAdapt = new System.Windows.Forms.Button();
+            this.tb_format = new System.Windows.Forms.TextBox();
+            this.groupBox_Output.SuspendLayout();
+            this.groupBox_Var.SuspendLayout();
+            this.groupBox_SQL.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cb_pinyin
+            // 
+            this.cb_pinyin.AutoSize = true;
+            this.cb_pinyin.Location = new System.Drawing.Point(494, 34);
+            this.cb_pinyin.Name = "cb_pinyin";
+            this.cb_pinyin.Size = new System.Drawing.Size(48, 16);
+            this.cb_pinyin.TabIndex = 16;
+            this.cb_pinyin.Text = "拼音";
+            this.cb_pinyin.UseVisualStyleBackColor = true;
+            // 
+            // bt_excute
+            // 
+            this.bt_excute.Location = new System.Drawing.Point(494, 56);
+            this.bt_excute.Name = "bt_excute";
+            this.bt_excute.Size = new System.Drawing.Size(46, 382);
+            this.bt_excute.TabIndex = 15;
+            this.bt_excute.Text = "走你";
+            this.bt_excute.UseVisualStyleBackColor = true;
+            // 
+            // groupBox_Output
+            // 
+            this.groupBox_Output.Controls.Add(this.tb_output);
+            this.groupBox_Output.Location = new System.Drawing.Point(546, 12);
+            this.groupBox_Output.Name = "groupBox_Output";
+            this.groupBox_Output.Size = new System.Drawing.Size(279, 432);
+            this.groupBox_Output.TabIndex = 14;
+            this.groupBox_Output.TabStop = false;
+            this.groupBox_Output.Text = "输出结果";
+            // 
+            // tb_output
+            // 
+            this.tb_output.Location = new System.Drawing.Point(6, 20);
+            this.tb_output.Multiline = true;
+            this.tb_output.Name = "tb_output";
+            this.tb_output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_output.Size = new System.Drawing.Size(267, 406);
+            this.tb_output.TabIndex = 0;
+            // 
+            // groupBox_Var
+            // 
+            this.groupBox_Var.Controls.Add(this.tb_var);
+            this.groupBox_Var.Location = new System.Drawing.Point(271, 12);
+            this.groupBox_Var.Name = "groupBox_Var";
+            this.groupBox_Var.Size = new System.Drawing.Size(217, 432);
+            this.groupBox_Var.TabIndex = 13;
+            this.groupBox_Var.TabStop = false;
+            this.groupBox_Var.Text = "占位字符串，分号分隔";
+            // 
+            // tb_var
+            // 
+            this.tb_var.Location = new System.Drawing.Point(6, 20);
+            this.tb_var.Multiline = true;
+            this.tb_var.Name = "tb_var";
+            this.tb_var.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_var.Size = new System.Drawing.Size(205, 406);
+            this.tb_var.TabIndex = 0;
+            // 
+            // groupBox_SQL
+            // 
+            this.groupBox_SQL.Controls.Add(this.cbb_template);
+            this.groupBox_SQL.Controls.Add(this.bt_selfAdapt);
+            this.groupBox_SQL.Controls.Add(this.tb_format);
+            this.groupBox_SQL.Location = new System.Drawing.Point(12, 12);
+            this.groupBox_SQL.Name = "groupBox_SQL";
+            this.groupBox_SQL.Size = new System.Drawing.Size(258, 432);
+            this.groupBox_SQL.TabIndex = 12;
+            this.groupBox_SQL.TabStop = false;
+            this.groupBox_SQL.Text = "格式化字符串（{序号从0开始}）";
+            // 
+            // cbb_template
+            // 
+            this.cbb_template.FormattingEnabled = true;
+            this.cbb_template.Location = new System.Drawing.Point(7, 20);
+            this.cbb_template.Name = "cbb_template";
+            this.cbb_template.Size = new System.Drawing.Size(164, 20);
+            this.cbb_template.TabIndex = 2;
+            // 
+            // bt_selfAdapt
+            // 
+            this.bt_selfAdapt.Location = new System.Drawing.Point(177, 18);
+            this.bt_selfAdapt.Name = "bt_selfAdapt";
+            this.bt_selfAdapt.Size = new System.Drawing.Size(75, 23);
+            this.bt_selfAdapt.TabIndex = 1;
+            this.bt_selfAdapt.Text = "应用模板";
+            this.bt_selfAdapt.UseVisualStyleBackColor = true;
+            // 
+            // tb_format
+            // 
+            this.tb_format.Location = new System.Drawing.Point(6, 44);
+            this.tb_format.Multiline = true;
+            this.tb_format.Name = "tb_format";
+            this.tb_format.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_format.Size = new System.Drawing.Size(246, 382);
+            this.tb_format.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 454);
+            this.Controls.Add(this.cb_pinyin);
+            this.Controls.Add(this.bt_excute);
+            this.Controls.Add(this.groupBox_Output);
+            this.Controls.Add(this.groupBox_Var);
+            this.Controls.Add(this.groupBox_SQL);
+            this.Name = "MainForm";
+            this.Text = "要多少有多少的批量字符串生成器";
+            this.groupBox_Output.ResumeLayout(false);
+            this.groupBox_Output.PerformLayout();
+            this.groupBox_Var.ResumeLayout(false);
+            this.groupBox_Var.PerformLayout();
+            this.groupBox_SQL.ResumeLayout(false);
+            this.groupBox_SQL.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cb_pinyin;
+        private System.Windows.Forms.Button bt_excute;
+        private System.Windows.Forms.GroupBox groupBox_Output;
+        private System.Windows.Forms.TextBox tb_output;
+        private System.Windows.Forms.GroupBox groupBox_Var;
+        private System.Windows.Forms.TextBox tb_var;
+        private System.Windows.Forms.GroupBox groupBox_SQL;
+        private System.Windows.Forms.ComboBox cbb_template;
+        private System.Windows.Forms.Button bt_selfAdapt;
+        private System.Windows.Forms.TextBox tb_format;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BatchString.Classess;
+
+namespace BatchString
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// 自适应按钮状态
+        /// </summary>
+        private enum SelfAdaptBTState
+        {
+            应用模板, 新增模板, 修改模板, 删除模板
+        }
+        /// <summary>
+        /// 当前状态
+        /// </summary>
+        private SelfAdaptBTState state;
+
+#if DEBUG
+        private static readonly string jsonPath = @"D:\Template\Template.json";
+#else
+        private static readonly string jsonPath = @".\Template\Template.json";
+#endif
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            cbb_template.DropDown +=
+                (object sender, EventArgs e) =>
+                {
+                    cbb_template.DataSource = TemplateManager.Instance.LoadTemplates(jsonPath);  //显示到下拉单选框;
+                    tb_format.Clear();
+                };
+
+            cbb_template.TextChanged +=
+                (object sender, EventArgs e) =>
+                {
+                    if (TemplateManager.Instance.ContainsTemplate(cbb_template.Text))
+                    {
+                        if (tb_format.Text == string.Empty)
+                            SetState(SelfAdaptBTState.应用模板);  //存在模板 && 格式化文本框为空 - 应用
+                        else
+                            SetState(SelfAdaptBTState.修改模板);  //存在模板 && 格式化文本框不为空 - 修改
+                    }
+                    else
+                    {
+                        if (cbb_template.Text.Length > 3)
+                        {
+                            int divide = cbb_template.Text.Length - 3;
+                            string prefix = cbb_template.Text.Substring(0, divide);
+                            string suffix = cbb_template.Text.Substring(divide);
+                            if (TemplateManager.Instance.ContainsTemplate(prefix) && suffix == "-rm")
+                                SetState(SelfAdaptBTState.删除模板);  //存在模板名称 && 存在'-rm'后缀 - 删除 
+                        }
+                        else
+                        {
+                            SetState(SelfAdaptBTState.新增模板);  //不存在模板 - 新增
+                        }
+                    }
+                };
+
+            tb_format.TextChanged +=
+                (object sender, EventArgs e) =>
+                {
+                    if (cbb_template.Text != string.Empty && TemplateManager.Instance.ContainsTemplate(cbb_template.Text))
+                        SetState(SelfAdaptBTState.修改模板);
+                };
+
+            bt_selfAdapt.Click +=
+                 (object sender, EventArgs e) =>
+                 {
+                     switch (state)
+                     {
+                         case SelfAdaptBTState.应用模板:
+                             string tempName = cbb_template.Text;
+                             if (TemplateManager.Instance.GetTemplate(tempName, out string template))
+                             {
+                                 tb_format.Text = template;
+                                 if (tempName == "示例模板")
+                                 {
+                                     tb_var.Text = "1\r\n2\r\n3\r\n" +
+                                     ";\r\n" +
+                                     "在这个文本框中输入格式化字符中的占位符，一行代表重复一次。\r\n" +
+                                     "如果存在多个占位符，用单独的一行分号分割。\r\n" +
+                                     "现在点击右边的'走你'按钮试试看吧！";
+                                 }
+                             }
+                             else
+                                 MessageBox.Show("模板应用失败");
+                             break;
+
+                         case SelfAdaptBTState.新增模板:
+                             if (TemplateManager.Instance.AddTemplate(cbb_template.Text, tb_format.Text, jsonPath))
+                                 MessageBox.Show("成功新增一条模板");
+                             else
+                                 MessageBox.Show("模板新增失败");
+                             break;
+
+                         case SelfAdaptBTState.修改模板:
+                             if (TemplateManager.Instance.ModifyTemplate(cbb_template.Text, tb_format.Text, jsonPath))
+                                 MessageBox.Show("成功修改一条模板");
+                             else
+                                 MessageBox.Show("模板修改失败");
+                             break;
+
+                         case SelfAdaptBTState.删除模板:
+                             if (TemplateManager.Instance.RemoveTemplate(cbb_template.Text.Substring(0, cbb_template.Text.Length - 3), jsonPath))
+                                 MessageBox.Show("成功删除一条模板");
+                             else
+                                 MessageBox.Show("删除模板失败");
+                             break;
+
+                         default: break;
+                     }
+                 };
+
+            bt_excute.Click +=
+                (object sender, EventArgs e) =>
+                {
+                        tb_output.Text = Factory_BatchString.Instance.GetString(tb_format.Text, tb_var.Text, cb_pinyin.Checked);  //批量生成
+                };
+
+        }
+
+        /// <summary>
+        /// 设置模板状态
+        /// </summary>
+        /// <param name="state"></param>
+        private void SetState(SelfAdaptBTState state)
+        {
+            this.state = state;
+            bt_selfAdapt.Text = state.ToString();
+        }
+    }
+}
